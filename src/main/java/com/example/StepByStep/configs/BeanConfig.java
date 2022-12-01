d3fc7029160ba6dfd2356804dbbc01b38737c41b
W0l0dymyr <DjVovic@gmail.com>
2022-12-01 10:28:17 +0200

    fixed deprecated code

--- src/main/java/com/example/StepByStep/configs/BeanConfig.java
+++ src/main/java/com/example/StepByStep/configs/BeanConfig.java
@@ -2,15 +2,16 @@ package com.example.StepByStep.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new MessageDigestPasswordEncoder("MD5");
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
     }
+
 
 }
--- src/main/java/com/example/StepByStep/configs/WebSecurityConfig.java
+++ src/main/java/com/example/StepByStep/configs/WebSecurityConfig.java
@@ -2,36 +2,42 @@ package com.example.StepByStep.configs;
 
 import com.example.StepByStep.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-@Autowired
-private UserService userService;
+public class WebSecurityConfig {
+    @Autowired
+    private UserService userService;
 
-@Autowired
-private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/registration", "/activate/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                 .and().logout().permitAll();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService).passwordEncoder(passwordEncoder)
+                .and().build();
     }
 }
